ab1cc7a434835142ac638f44d92116f0f2187fac
HannahCiro <hannah.fadu@gmail.com>
2022-09-04 19:24:53 -0300

    Corrección entrega simulador

--- index.js
+++ index.js
@@ -1,57 +1,109 @@
 alert ("Bienvenido a TENTO SUSHI");
-let saludo = prompt ("Quieres conocer nuestro menú?");
-while(saludo != "si"){
-    alert("Entonces te invitamos a que conozcas nuestra pagina y te antojes de todo lo rico que tenemos para vos y tus amigues");
-    saludo = prompt ("Quieres conocer nuestro menú?");
-}
 
-alert("A empezar entonces");
+let usuario = prompt("¿Como te llamas?");
+let mail = prompt("Cual es tu mail?");
+let tabla = 0;
+let valComida = 0;
+let valBebida = 0;
+let valortotal = 0;
 
-
-function menu () {
-let menuSushi;
-do {
-    menuSushi = parseInt(prompt("Los especiales del dia son:\n1: 1 Tabla de 60 piezas variadas de salmón y pesca del dia $4000. \n2: 2 Tablas de 30 piezas, una de salmón y otra vegetariana + botella de vino blanco $6000. \n3: 1 Tabla de 30 piezas vegetarianas y venaganas + 2 latas de combucha $3000. \n4: 1 Tabla individual de 12 piezas + 8 piezas de regalo $2800" ))
-}
-while (menuSushi!=1 && menuSushi!=2 && menuSushi!=3 && menuSushi!=4)
-switch (menuSushi) {
-    case 1:
-        return "tabla 1";
-    case 2:
-        return "tabla 2";
-    case 3:
-        return "tabla 3";
-    case 4:
-        return "tabla 4";
+function confirDatos () {
+    let confirUsuario = prompt(`${usuario} ¿estas preparad@ para una nueva experiencía?`);
+    if (confirUsuario.toLowerCase () === "si"){
+    alert ("Genial, continuemos... No te vas a arrepentir");
+    }else{
+    alert (`No pasa nada ${usuario} .  Te invitamos a que des un recorrido por nuestra pagina. Nos vemos pronto`);
     }
 }
+confirDatos();
 
-function precio (menuSushi) {
-    if (menuSushi === "tabla 1") {
-        return 4000;
+function pedidoComidas() {
+    let menuComidas;
+    do { 
+        menuComidas = parseInt(prompt("NUESTRAS TABLAS DEL DÍA SON: \n\
+        1: Tablas de 60 piezas variadas de salmón y pesca del dia $9520. \n\
+        2: Tablas de 30 piezas, una de salmón y otra vegetariana $9000. \n\
+        3: Tablas de 30 piezas vegetarianas y veganas $4000. \n\
+        4: Tablas individual de 12 piezas + 8 piezas de regalo $2440"));
     }
-    else if (menuSushi === "tabla 2") {
-        return 6000;
-    }
-    else if (menuSushi === "Promo 3") {
-        return 3000;
-    }
-    else if (menuSushi === "Promo 4") {
-        return 2800;
-    }
-    else {
-        console.log("No es una opción válida");
-    }
+    while (menuComidas!=1 && menuComidas!=2 && menuComidas!=3 && menuComidas!=4);
+    switch (menuComidas){
 
+        case 1:
+        valComida += 9520;
+        return (valComida);
+        break;
+
+        case 2: 
+        valComida += 9000;
+        return (valComida);
+        break;
+
+        case 3: 
+        valComida += 9520;
+        return (valComida);
+        break;
+
+        case 4: 
+        valComida += 2440;
+        return (valComida);
+        break;
+
+        default: alert("No es una opcion valida");
+        break;
+    }
 }
 
-alert("Ha sido una gran elección vas a disfrutar de " + sushi + "con un costo total de " + precio + "Por se tu primera compra te damos un descuento de $1000");
+function pedidoBebidas() {
+let menuBebidas;
 
-let descuento = 1000
-const resta = (precio, descuento) => precio - descuento;
-const iva = x => x * 0.21;
+    do {
+        menuBebidas = parseInt(prompt("NUESTRAS BEBIDAS DEL DÍA SON: \n\
+        1: 1 Botella de vino tinto Malbec $2000. \n\
+        2: 2 Latas de combucha $1500. \n\
+        3: 2 Botellas de cerveza Corona $2000. \n\
+        4: 1 Botella de gaseosa + 1 botella de agua $1000"));
+    }
+    while (menuBebidas!=1 && menuBebidas!=2 && menuBebidas!=3 && menuBebidas!=4);
+            
+    switch (menuBebidas){
 
-let precioFinal = resta((precio, descuento) * iva );
-alert ("Puede pasar por su pedido en 40min, el saldo total es" + precioFinal);
+        case 1: 
+        valBebida += 2000;
+        return(valBebida);
+        break;
+
+        case 2: 
+        valBebida += 1500;
+        return(valBebida);
+        break;
+
+        case 3: 
+        valBebida += 2000;
+        return(valBebida);
+        break;
+
+        case 4: 
+        valBebida += 1000;
+        return(valBebida);
+        break;
+
+        default: alert("No es una opcion valida");
+        break;
+    }
+}
+       
+let resumenPago = pedidoComidas() + pedidoBebidas();
 
 
+function validarCompra(){
+    let validar = (prompt("Has hecho una gran elección. Desea pagar con: \n\ Tajeta + $300 de recargo \n\ Efectivo"))
+    if (validar.toLowerCase() === "tarjeta"){
+        alert(resumenPago + 300); 
+    } else {
+        alert(resumenPago);
+    }
+}
+validarCompra();
+ 
+let mensaje = alert(`Muchas gracias por su compra. Le estaremos enviando la factura a su casilla de correo ${mail}`);
